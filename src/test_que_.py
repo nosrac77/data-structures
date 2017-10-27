6fd43a2ad695e9ae3164fce5357de53711e97241
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-26 19:39:41 -0700

    finishing queue tests

--- src/test_que_.py
+++ src/test_que_.py
@@ -6,14 +6,6 @@ from que_ import Q
 q = Q()
 
 
-def test_empty_queue():
-    """Test that an empty queue has no len, test empty peek."""
-    assert q.peek() is None
-    assert len(q) == 0
-    with pytest.raises(IndexError):
-        q.pop()
-
-
 def test_enqueue():
     """Test that enqueue function works."""
     q.enqueue(1)
@@ -21,7 +13,7 @@ def test_enqueue():
     assert len(q) == 1
     q.enqueue(2)
     assert q.peek() == 1
-    q.enque(3)
+    q.enqueue(3)
     assert len(q) == 3
 
 
@@ -36,14 +28,39 @@ def test_dequeue():
     assert q.dequeue() == 3
 
 
-# def test_push_one_node_into_doubly_linked_list():
-#     """Test that pushing one value adds it to front of doubly linked list."""
-#     l = DoublyLinked()
-#     l.push(0)
-#     assert l.head.data == 0
-#     assert l.tail.data == 0
-#     assert l.head.next_node is None
-#     assert l.head.prev_node is None
-#     assert l.tail.next_node is None
-#     assert l.tail.prev_node is None
-#     assert len(l) == 1
+def test_dequeue_from_empty():
+    """Test dequeue from empty queue."""
+    with pytest.raises(IndexError):
+        q.dequeue()
+
+
+def test_peek():
+    """Test that peek works."""
+    q.enqueue(1)
+    assert q.peek() == 1
+    q.enqueue(2)
+    assert q.peek() == 1
+    q.dequeue()
+    assert q.peek() == 2
+
+
+def test_peek_on_empty_queue():
+    """Test edge case, calling peak on empty queue."""
+    assert q.peek() is None
+
+
+def test_len_empty_queue():
+    """Test that an empty queue has no len."""
+    assert len(q) == 0
+
+
+def test_len():
+    """Test that len function works."""
+    q.enqueue(500)
+    assert len(q) == 1
+    q.enqueue(1000)
+    assert len(q) == 2
+    q.enqueue(1500)
+    assert len(q) == 3
+    q.dequeue()
+    assert len(q) == 2
