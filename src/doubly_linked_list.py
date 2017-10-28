1921080523c1e442514b70e6799773701f650e0f
nosrac77 <carson.newton@outlook.com>
2017-10-27 18:35:06 -0700

    updated doubly linked list to ensure queue functionality

--- src/doubly_linked_list.py
+++ src/doubly_linked_list.py
@@ -26,12 +26,13 @@ class DoublyLinked(object):
         """Emulate LinkedList pop method."""
         if self.head is None:
             raise IndexError('List is empty.')
-        output = self.head.data
         if self.head.next_node is None:
             self.tail = None
-            self.head = None
-        else:
-            self.head = self.head.next_node
+        if self.head.next_node.next_node is None:
+            self.tail = self.head
+        output = self.head.data
+        self.head = self.head.next_node
+        if self.head:
             self.head.prev_node = None
         self._counter -= 1
         return output
@@ -39,13 +40,18 @@ class DoublyLinked(object):
     def remove(self, val):
         """Remove given node from linked list."""
         current_node = self.head
-        while current_node.next_node is not None:
+        while current_node is not None:
+            if current_node is self.tail:
+                self.tail = self.tail.prev_node
+                current_node.prev_node.next_node = None
+                return val
             if current_node.data == val:
-                output = current_node.data
-                self.head = self.head.next_node
-                self.head.prev_node = None
+                current_node.prev_node.next_node = current_node.next_node
+                current_node.next_node.prev_node = current_node.prev_node
                 self._counter -= 1
-                return output
+                return val
+            if current_node.next_node is None:
+                raise IndexError('Input value not in DoublyLinkedList.')
             current_node = current_node.next_node
 
     def __len__(self):
@@ -57,7 +63,7 @@ class DoublyLinked(object):
         return self.remove(self.tail.data)
 
     def append(self, val):
-        """Appends to end of list."""
+        """Append to end of list."""
         if self.head is None:
             self.push(val)
             return
@@ -66,10 +72,26 @@ class DoublyLinked(object):
         self.tail = curr
         self._counter += 1
 
+    def display(self):
+        """Print properly formatted doubly linked list."""
+        start_paren = "("
+        if self.head is None:
+            return "()"
+        current_node = self.head
+        while current_node:
+            if current_node.next_node is None:
+                start_paren += str(current_node.data) + ")"
+                return start_paren
+            else:
+                start_paren += str(current_node.data) + ", "
+                current_node = current_node.next_node
+
 
 class Node(object):
     """Double List Node class."""
+
     def __init__(self, val, next_node=None, prev_node=None):
+        """Initialize Node class instance."""
         self.data = val
         self.next_node = next_node
         self.prev_node = prev_node
