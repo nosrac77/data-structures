9f9790a7cd59ddc6f01da60f4b977fbe140c7e71
nosrac77 <carson.newton@outlook.com>
2017-11-01 13:51:51 -0700

    wrote for loop, commented out code

--- src/bin_heap.py
+++ src/bin_heap.py
@@ -42,18 +42,26 @@ class MaxHeap(object):
             raise IndexError('Cannot pop from an empty list.')
         head_val = self.heap[0]
         self.heap.pop(0)
-        curr_idx = len(self.heap) - 1
-        parent_idx = self.find_parent(curr_idx)
-        while self.heap[curr_idx] > self.heap[parent_idx]:
-            parent_val = self.heap[parent_idx]
-            self.heap[parent_idx] = self.heap[curr_idx]
-            self.heap[curr_idx] = parent_val
-            curr_idx = self.find_parent(curr_idx)
-            parent_idx = self.find_parent(curr_idx)
-        if self.heap[curr_idx] > self.heap[0]:
-            head_val = self.heap[0]
-            self.heap[0] = self.heap[curr_idx]
-            self.heap[curr_idx] = head_val
+
+        for idx in len(self.heap):
+            if self.heap[idx] < self.heap.find_children(idx)[0] or self.heap[idx] < self.heap.find_children(idx)[1]:
+                higher_val = max(self.find_children(idx))
+                self.heap[idx] = higher_val
+        return head_val
+
+        # curr_idx = len(self.heap) - 1
+        # parent_idx = self.find_parent(curr_idx)
+
+        # while self.heap[curr_idx] > self.heap[parent_idx]:
+        #     parent_val = self.heap[parent_idx]
+        #     self.heap[parent_idx] = self.heap[curr_idx]
+        #     self.heap[curr_idx] = parent_val
+        #     curr_idx = self.find_parent(curr_idx)
+        #     parent_idx = self.find_parent(curr_idx)
+        # if self.heap[curr_idx] > self.heap[0]:
+        #     head_val = self.heap[0]
+        #     self.heap[0] = self.heap[curr_idx]
+        #     self.heap[curr_idx] = head_val
         # for idx, num in enumerate(self.heap):
         #     if self.heap[idx] < self.find_children(idx)[0] or self.heap[idx] < self.find_children(idx)[1]:
         #         higher_val = max(self.find_children(idx))
@@ -67,4 +75,4 @@ class MaxHeap(object):
         #     head_val = self.heap[0]
         #     self.heap[0] = self.heap[curr_idx]
         #     self.heap[curr_idx] = head_val
-        return head_val
+        # return head_val
