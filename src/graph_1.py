c8ac7cabc64653b1ece799d6a7ff9b92f3bfd7b7
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-02 14:22:14 -0700

    first 5 tests passing.

--- src/graph_1.py
+++ src/graph_1.py
@@ -2,28 +2,24 @@
 
 
 class G(object):
-    """Class that creates instance of doubly linked list."""
+    """Class that creates instance of a graph."""
 
     def __init__(self):
         """Initialize Graph instance."""
         self.all_edges = []
         self.all_nodes = []
 
-
     def nodes(self):
-        """Returns a list of all nodes."""
+        """Return a list of all nodes."""
         return self.all_nodes
 
-
-     def edges(self):
-        """Returns a list of all edges."""
+    def edges(self):
+        """Return a list of all edges."""
         return self.all_edges
-
 
     def add_node(self, val):
         """Add new node to graph."""
-        self.all_nodes.push(Node(val))
-        
+        self.all_nodes.append(Node(val))
 
     def add_edge(self, val1, val2):
         """Add new node to graph."""
@@ -35,7 +31,6 @@ class G(object):
             if self.all_edges[edge] == (val1, val2):
                 self.all_edges[edge] = (val1, val2)
 
-
     def del_node(self, val):
         """Remove given node from graph."""
         if val in self.all_nodes:
@@ -43,15 +38,12 @@ class G(object):
         else:
             raise IndexError('This node is not in the graph.')
 
-
-
     def del_edge(self, val1, val2):
         """Remove given edge from graph."""
         if (val1, val2) in self.all_edges:
             self.all_edges.remove((val1, val2))
         else:
             raise IndexError('This edge is not in the graph.')
-
 
     def has_node(self, val):
         """Return Boolean: is "val" node in graph."""
@@ -69,7 +61,7 @@ class G(object):
 
     def adjacent(self, val1, val2):
         """Return whether val1 and val2 have a connecting edge."""
-        if Node(val1) not in self.all_nodes or Node(val2) not in self.all_nodes:
+        if Node(val1) not in self.all_nodes or Node(val2) not in self.all_nodesgr:
             return False
         for edge in self.all_edges:
             if edge == (val1, val2) or edge == (val2, val1):
---
+++ src/test_graph_1.py
@@ -0,0 +1,50 @@
+"""Tests for Graph 1."""
+
+import pytest
+
+
+@pytest.fixture
+def init_g():
+    """Fixture for initializing g()."""
+    from graph_1 import G
+    g = G()
+    return g
+
+
+def test_newly_initialized_graph_all_nodes(init_g):
+    """Test that an empty graph has all_nodes."""
+    assert init_g.all_nodes == []
+    assert len(init_g.all_nodes) == 0
+
+
+def test_newly_initialized_graph_all_edges(init_g):
+    """Test that an empty graph has all_edges."""
+    assert init_g.all_edges == []
+    assert len(init_g.all_edges) == 0
+
+
+def test_new_graph_nodes_function(init_g):
+    """Test that an empty graph has no nodes."""
+    assert init_g.nodes() == []
+
+
+def test_new_graph_edges_function(init_g):
+    """Test that an empty graph has no edges."""
+    assert init_g.edges() == []
+
+
+def test_add_nodes_to_empty_graph(init_g):
+    """Test that the add_node fn works on an empty graph."""
+    init_g.add_node(500)
+    assert len(init_g.all_nodes) == 1
+    assert init_g.nodes()[0].data == 500
+
+
+# def test_size_of_deque(init_g):
+#     """Test that the size method is functional."""
+#     d = Deque()
+#     d.append(1)
+#     assert d.size() == 1
+#     d.append(2)
+#     d.append(3)
+#     assert d.size() == 3
