6c23b96e03a461b41444913f205c99fe0fd5ea7c
nosrac77 <carson.newton@outlook.com>
2017-11-05 14:57:57 -0800

    binheap is now fully functional with 97% coverage and 12 tests

--- src/bin_heap.py
+++ src/bin_heap.py
@@ -5,23 +5,24 @@ class MaxHeap(object):
     """Class that creates a max binary heap, optionally accepting an
     iterable of values."""
 
+    def __init__(self, iterable=[]):
+        """Create instance of MaxHeap class."""
+        self.heap = sorted(iterable, reverse=True)
+
     def find_children(self, idx):
+        """Return tuple of children values given parent value."""
         return (self.heap[idx * 2 + 1], self.heap[idx * 2 + 2])
 
     def find_parent(self, idx):
+        """Return index of parent value given child value."""
         if idx % 2 == 0:
             parent_idx = idx / 2 - 1
         elif idx % 2 == 1:
             parent_idx = (idx - 1) / 2
-        return parent_idx
+        return int(parent_idx)
 
-    def __init__(self, iterable=[]):
-        """Create an instance of MaxHeap class."""
-        self.heap = sorted(iterable, reverse=True)
-
-    def push(self, val):
-        """Add value to end of heap."""
-        self.heap.append(val)
+    def bubble_up(self):
+        """Sort Max Heap from end of heap."""
         curr_idx = len(self.heap) - 1
         parent_idx = self.find_parent(curr_idx)
         while self.heap[curr_idx] > self.heap[parent_idx]:
@@ -36,43 +37,49 @@ class MaxHeap(object):
             self.heap[curr_idx] = head_val
         return self.heap
 
+    def sort_down(self):
+        """Sort Max Heap from top downward."""
+        if len(self.heap) == 3:
+            first_child = self.heap[1]
+            second_child = self.heap[2]
+            biggest_child = max(first_child, second_child)
+            if biggest_child > self.heap[0]:
+                self.heap[self.heap.index(biggest_child)] = self.heap[0]
+                self.heap[0] = biggest_child
+        elif len(self.heap) > 4:
+            for idx in range((len(self.heap) // 2) - 1):
+                first_child = self.find_children(idx)[0]
+                second_child = self.find_children(idx)[1]
+                if self.heap[idx] < first_child or self.heap[idx] < second_child:
+                    higher_val = max(first_child, second_child)
+                    lower_val = self.heap[idx]
+                    self.heap[self.heap.index(higher_val)] = lower_val
+                    self.heap[idx] = higher_val
+
+    def push(self, val):
+        """Add value to end of heap."""
+        self.heap.append(val)
+        self.bubble_up()
+
     def pop(self):
         """Remove and return value from top of heap."""
         if len(self.heap) == 0:
             raise IndexError('Cannot pop from an empty list.')
+        elif len(self.heap) == 1:
+            return self.heap.pop()
+        elif len(self.heap) == 2:
+            return self.heap.pop(0)
+        elif len(self.heap) == 3:
+            head_val = self.heap[0]
+            self.heap.pop(0)
+            if self.heap[0] < self.heap[1]:
+                self.heap[0] = self.heap[1]
+                self.heap[1] = self.heap[0]
+            return head_val
         head_val = self.heap[0]
         self.heap.pop(0)
 
-        for idx in len(self.heap):
-            if self.heap[idx] < self.heap.find_children(idx)[0] or self.heap[idx] < self.heap.find_children(idx)[1]:
-                higher_val = max(self.find_children(idx))
-                self.heap[idx] = higher_val
+        self.sort_down()
+        self.bubble_up()
+
         return head_val
-
-        # curr_idx = len(self.heap) - 1
-        # parent_idx = self.find_parent(curr_idx)
-
-        # while self.heap[curr_idx] > self.heap[parent_idx]:
-        #     parent_val = self.heap[parent_idx]
-        #     self.heap[parent_idx] = self.heap[curr_idx]
-        #     self.heap[curr_idx] = parent_val
-        #     curr_idx = self.find_parent(curr_idx)
-        #     parent_idx = self.find_parent(curr_idx)
-        # if self.heap[curr_idx] > self.heap[0]:
-        #     head_val = self.heap[0]
-        #     self.heap[0] = self.heap[curr_idx]
-        #     self.heap[curr_idx] = head_val
-        # for idx, num in enumerate(self.heap):
-        #     if self.heap[idx] < self.find_children(idx)[0] or self.heap[idx] < self.find_children(idx)[1]:
-        #         higher_val = max(self.find_children(idx))
-        #         self.heap[idx] = higher_val
-        # while self.heap[curr_idx] > self.heap[parent_idx]:
-        #     parent_val = self.heap[parent_idx]
-        #     self.heap[parent_idx] = self.heap[curr_idx]
-        #     self.heap[curr_idx] = parent_val
-        #     curr_idx = self.find_parent(curr_idx)
-        # if self.heap[curr_idx] > self.heap[0]:
-        #     head_val = self.heap[0]
-        #     self.heap[0] = self.heap[curr_idx]
-        #     self.heap[curr_idx] = head_val
-        # return head_val
--- src/test_bin_heap.py
+++ src/test_bin_heap.py
@@ -1,73 +1,155 @@
 """Functions that test methods of MaxHeap class."""
-import pytest
 from bin_heap import MaxHeap
 
 
-@pytest.fixture
-def empty_max_heap():
-    """Create an empty MaxHeap."""
-    return MaxHeap()
-
-
-def test_heap_empty_upon_init(empty_max_heap):
-    """Function to test heap is empty list upon init."""
-    m = empty_max_heap
-    assert len(m.heap) == 0
-    assert isinstance(m.heap, list)
-
-
-def test_push_method_inserts_node_containing_val_at_head(empty_max_heap):
-    """Function to test push method adds new value to heap."""
-    m = empty_max_heap
-    m.push(1)
-    assert m.heap[0] == 1
-
-
-def test_heap_unpacks_iterable_upon_init():
-    """Function to test optional iterable enters into heap list upon init."""
-    m = MaxHeap([1, 2, 3])
-    assert m.heap[0] == 3
-    assert m.heap[1] == 2
-    assert m.heap[2] == 1
-
-
-def test_heap_pop_method_removes_and_returns_value_at_heap_head(empty_max_heap):
-    """Function to test if pop method remove and return head value of heap."""
-    m = empty_max_heap
-    m.push(1)
-    assert m.pop() == 1
+def test_empty_bin_heap():
+    """Test that an empty MaxHeap has len(0)."""
+    m = MaxHeap()
     assert len(m.heap) == 0
 
 
-def test_heap_maintains_max_heap_when_values_are_pushed_in(empty_max_heap):
-    """Function to test if heap maintains max when multiple values are pushed
-    in."""
-    m = empty_max_heap
+def test_pop_from_empty_heap():
+    """Test that you can't pop from empty heap."""
+    m = MaxHeap()
+    try:
+        m.pop()
+    except IndexError:
+        pass
+
+
+def test_push_to_heap():
+    """Test that the push method is functional."""
+    m = MaxHeap()
     m.push(1)
     m.push(2)
     m.push(3)
-    m.push(500)
     m.push(4)
-    assert m.heap[0] == 500
-    assert m.heap[1] == 4
-    assert m.heap[2] == 1
-    assert m.heap[3] == 2
-    assert m.heap[4] == 3
+    m.push(30)
+    assert m.heap == [30, 4, 2, 1, 3]
 
 
-def test_heap_maintains_max_heap_when_values_are_popped_out(empty_max_heap):
-    """Function to test if heap maintatins max when pop method removes head value
-    from heap."""
-    m = empty_max_heap
-    m.push(1)
-    m.push(2)
-    m.push(3)
-    m.push(500)
-    m.push(4)
-    m.push(5)
+def test_find_children_returns_tuple_of_correct_values_given_parent_value_idx():
+    """Test that the push method is functional."""
+    m = MaxHeap()
+    m.push(20)
+    m.push(15)
+    m.push(14)
+    assert m.heap[0] == 20
+    assert m.find_children(0) == (15, 14)
+    m.push(10)
+    m.push(9)
+    assert m.heap[1] == 15
+    assert m.find_children(1) == (10, 9)
+
+
+def test_find_parent_returns_idx_of_correct_parent():
+    """Test that the push method is functional."""
+    m = MaxHeap()
+    m.push(20)
+    m.push(15)
+    m.push(14)
+    parent_correct_idx = m.heap.index(20)
+    assert parent_correct_idx == 0
+    assert m.heap[parent_correct_idx] == 20
+    assert m.find_children(parent_correct_idx) == (15, 14)
+    assert m.heap[1] == 15
+    assert m.find_parent(1) == parent_correct_idx
+    m.push(10)
+    m.push(9)
+    second_parent_correct_idx = m.heap.index(15)
+    assert second_parent_correct_idx == 1
+    assert m.heap[second_parent_correct_idx] == 15
+    assert m.find_children(second_parent_correct_idx) == (10, 9)
+    assert m.heap[4] == 9
+    assert m.find_parent(4) == second_parent_correct_idx
+
+
+def test_pop_from_populated_heap():
+    """Test pop from populated heap."""
+    m = MaxHeap()
+    m.push(7)
+    m.push(30)
+    m.push(10)
+    m.push(20)
+    assert m.pop() == 30
+
+
+def test_push_swaps_head_val_if_new_children_vals_greater_than_head_val():
+    """Test push method swaps places with head value if head values children
+    are greater than head value."""
+    m = MaxHeap()
+    m.push(7)
+    assert m.heap == [7]
+    m.push(30)
+    assert m.heap == [30, 7]
+    m.push(50)
+    assert m.heap == [50, 7, 30]
+
+
+def test_heap_maintains_max_through_multiple_pushes():
+    """Test push method maintains max heap property when used multiple times."""
+    m = MaxHeap()
+    m.push(7)
+    m.push(30)
+    m.push(10)
+    m.push(20)
+    assert m.heap == [30, 20, 10, 7]
+    m.push(30)
+    assert m.heap == [30, 30, 10, 7, 20]
+    m.push(50)
+    assert m.heap == [50, 30, 30, 7, 20, 10]
+    m.push(90)
+    assert m.heap == [90, 30, 50, 7, 20, 10, 30]
+    m.push(9)
+    assert m.heap == [90, 30, 50, 9, 20, 10, 30, 7]
+
+
+def test_pop_multiple_times():
+    """Test pop method return and remove head value multiple times in a row."""
+    m = MaxHeap()
+    m.push(7)
+    m.push(30)
+    m.push(10)
+    m.push(20)
+    assert m.pop() == 30
+    assert m.pop() == 20
+    assert m.pop() == 10
+    assert m.pop() == 7
+
+
+def test_heap_instance_given_iterable_is_sorted_upon_init():
+    """Test pop from populated heap."""
+    m = MaxHeap([30, 50, 90, 7, 55, 20, 99, 4, 66])
+    assert m.heap == [99, 90, 66, 55, 50, 30, 20, 7, 4]
+
+
+def test_push_maintains_max_heap_when_instance_given_iterable_on_init():
+    """Test pop from populated heap."""
+    m = MaxHeap([30, 50, 90, 55, 20, 99, 66])
+    assert m.heap == [99, 90, 66, 55, 50, 30, 20]
+    m.push(100)
+    assert m.heap == [100, 99, 66, 90, 50, 30, 20, 55]
+
+
+def test_heap_maintains_max_through_multiple_pushes_and_pops():
+    """Test push method multiple times from populated heap."""
+    m = MaxHeap()
+    m.push(7)
+    m.push(30)
+    m.push(10)
+    m.push(20)
+    assert m.heap == [30, 20, 10, 7]
+    m.push(30)
+    assert len(m.heap) == 5
+    assert m.heap == [30, 30, 10, 7, 20]
     m.pop()
-    assert m.heap[0] == 5
-    assert m.heap[1] == 4
-    assert m.heap[2] == 2
-    assert m.heap[3] == 3
-    assert m.heap[4] == 1
+    assert m.heap == [30, 20, 7, 10]
+    m.push(50)
+    assert m.heap == [50, 30, 7, 10, 20]
+    m.push(35)
+    assert m.heap == [50, 30, 35, 10, 20, 7]
+    m.pop()
+    assert m.heap == [35, 30, 10, 20, 7]
+    m.pop()
+    m.pop()
+    assert m.heap == [20, 10, 7]
